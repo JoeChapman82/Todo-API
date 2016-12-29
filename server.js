905f1f3b16b48ec901f4999c559461f7d82d6cc4
Joe Chapman <contemno82@gmail.com>
2016-12-29 19:20:25 +0000

    add post for todos api

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-  id: 1,
-  description: 'Meet mum for lunch',
-  completed: false
-},
-{
-  id: 2,
-  description: 'Go to market',
-  completed: false
-},
-{
-  id: 3,
-  description: 'Complete task for node video',
-  comlpleted: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+// This from body-parser middlewhere to use req.body
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.send('Todo API Root');
@@ -23,26 +15,44 @@ app.get('/', function(req, res) {
 
 // GET request /todos
 app.get('/todos', function(req, res) {
-  return res.json(todos); // converts response to json (instead of using json stingify)
+  return res.json(todos);
+  // res.json converts response to json (shorter than JSON.stringify)
 });
 
 app.get('/todos/:id', function(req, res) {
+  /// GET /todos/:id (to get parts)
   var todoId = parseInt(req.params.id, 10);
+  // Use parseInt as req.params returns a string
   var matchedTodo;
     todos.forEach(function(todo) {
+      //where todo is the loop index item of the array the for each runs through
       if (todoId === todo.id) {
         matchedTodo = todo;
       }
     });
-
     if (matchedTodo) {
       res.json(matchedTodo);
     } else {
       res.status(404).send();
     }
-
 });
-/// GET /todos/:id    (to get parts of thing)
+
+// Challenge
+// Add the body to the todos array
+// along with whatevers set. Add an Id field and set
+// it to todoNextIdinto todos request
+// add id field
+// push body into array
+// return todo item
+
+// POST /todos
+app.post('/todos', function(req, res) {
+  var body = req.body;
+  body.id =  todoNextId++; // This sets value to body THEN increments the value
+  todos.push(body);
+res.json(body);
+});
+
 
 app.listen(PORT, function () {
   console.log('Express listening on port ' + PORT + '!');
