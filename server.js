55c6fb14112d6b5fb72dca5829ffe00bd4aaf774
Joe Chapman <contemno82@gmail.com>
2016-12-31 11:28:15 +0000

    convert get todos to sequelize

--- server.js
+++ server.js
@@ -16,20 +16,39 @@ app.get('/', function(req, res) {
 
 // GET /todos?completed=true&q=work
 app.get('/todos', function(req, res) {
-  var queryParams = req.query;
-  var filteredTodos = todos;
-  if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-    filteredTodos = _.where(filteredTodos, {completed: true});
-  } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-    filteredTodos = _.where(filteredTodos, {completed: false});
-  }
-   if (queryParams.hasOwnProperty('q') && queryParams.q.trim().length > 0) {
-     filteredTodos = _.filter(filteredTodos, function(todo){
-       return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase) > -1; });
-   }
-  return res.json(filteredTodos);
-  // res.json converts response to json (shorter than JSON.stringify)
+  var query = req.query;
+  var where = {};
+    if (query.hasOwnProperty('completed') && query.completed === 'true') {
+      where.completed = true;
+    } else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+      where.completed = false;
+    }
+    if (query.hasOwnProperty('q') && (query.q.length > 0)) {
+      where.description = {
+        $like: '%' + query.q + '%'
+      };
+    }
+  db.todo.findAll({where: where}).then(function(todos) {
+    res.json(todos);
+  }, function(e) {
+    res.status(500).send();
+  });
 });
+  // As above but using underscore with no db
+  //   var queryParams = req.query;
+//   var filteredTodos = todos;
+//   if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+//     filteredTodos = _.where(filteredTodos, {completed: true});
+//   } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+//     filteredTodos = _.where(filteredTodos, {completed: false});
+//   }
+//    if (queryParams.hasOwnProperty('q') && queryParams.q.trim().length > 0) {
+//      filteredTodos = _.filter(filteredTodos, function(todo){
+//        return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase) > -1; });
+//    }
+//   return res.json(filteredTodos);
+//   // res.json converts response to json (shorter than JSON.stringify)
+// });
 
 app.get('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id, 10);
