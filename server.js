9c6804201be2d5e33b260ddbf1798a2cb448e8a3
Joe Chapman <contemno82@gmail.com>
2016-12-31 12:50:01 +0000

    changed delete todos to sequelize

--- server.js
+++ server.js
@@ -92,14 +92,30 @@ res.json(body);
 // DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id, 10);
-  var matchedTodo = _.findWhere(todos, {id: todoId});
-  if (!matchedTodo) {
-    res.status(404).json({"error": "No Todo found with that id"});
-  } else {
-  todos = _.without(todos, matchedTodo);
-  res.json(matchedTodo);
-}
+  db.todo.destroy({
+    where: {
+      'id': todoId
+    }
+  }).then(function(rowsDeleted) {
+    if (rowsDeleted === 0) {
+      res.status(404).json({
+        error: 'No todo with that id'
+      });
+    } else {
+      res.status(204).send();
+    }
+  }, function() {
+    res.status(500).send();
+  });
 });
+//   var matchedTodo = _.findWhere(todos, {id: todoId});
+//   if (!matchedTodo) {
+//     res.status(404).json({"error": "No Todo found with that id"});
+//   } else {
+//   todos = _.without(todos, matchedTodo);
+//   res.json(matchedTodo);
+// }
+// });
 
 // PUT /todos/:id
 app.put('/todos/:id', function(req, res) {
